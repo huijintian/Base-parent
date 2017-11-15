2dccca6296a37b041669468567ad40ec9b4fd262
huijintian <wu750316@163.com>
2017-11-15 22:18:04 +0800

    add fileUtil

--- base-util/src/main/java/com/base/util/FileUtil.java
+++ base-util/src/main/java/com/base/util/FileUtil.java
@@ -1,7 +1,29 @@
 package com.base.util;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by mengtian on 2017/11/13
  */
 public class FileUtil {
+    public static List<String> listFile(String path, boolean fullPath) {
+        List<String> files = new ArrayList<>();
+        File p = new File(path);
+        if (!p.isDirectory()) {
+            return files;
+        }
+        File[] listFiles = p.listFiles();
+        for (File listFile : listFiles) {
+            if (listFile.isDirectory()) {
+                files.addAll(listFile(listFile.getAbsolutePath(), fullPath));
+            } else {
+                files.add(fullPath ?
+                        listFile.getAbsolutePath() :
+                        p.getName().concat(File.separator).concat(listFile.getName()));
+            }
+        }
+        return files;
+    }
 }
