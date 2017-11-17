16568084f05dde66eaf4018ad7bd7a00fadb1237
mengtian <wu750316@163.com>
2017-11-17 22:55:13 +0800

    add

--- leetcode/src/main/java/com/leetcode/Palindrome.java
+++ leetcode/src/main/java/com/leetcode/Palindrome.java
@@ -7,16 +7,27 @@ package com.leetcode;
  */
 public class Palindrome {
     public static void main(String[] args) {
-        int[] nums = new int[]{12321, 123};
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        int[] nums = new int[]{1000110001, 1000021, 1001, -2147447412, -2147483648, 12321, 123};
         for (int num : nums) {
-            System.out.println("num: " + num + "isPalindrome:" + isPalindrome(num));
+            System.out.println("num: " + num + " isPalindrome:" + isPalindrome(num));
         }
     }
 
+    /**
+     * leetcode discuss code
+     *
+     * @param x
+     * @return
+     */
     public static boolean isPalindrome(int x) {
-
-
-
-        return false;
+        if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+        int rev = 0;
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x = x / 10;
+        }
+        return (x == rev || x == rev / 10);
     }
 }
---
+++ leetcode/src/main/java/com/leetcode/RegularMatch.java
@@ -0,0 +1,14 @@
+package com.leetcode;
+
+/**
+ * Created by mengtian on 2017/11/16
+ */
+public class RegularMatch {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isMatching(String s, String p) {
+        return false;
+    }
+}
