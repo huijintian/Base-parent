3b7a761163be5713264dc275fc6f75744e51929c
huijintian <wu750316@163.com>
2017-11-18 22:40:07 +0800

    添加注释

--- dl4j/src/main/java/com/dl4j/word2vec/NovelWord2Vec.java
+++ dl4j/src/main/java/com/dl4j/word2vec/NovelWord2Vec.java
@@ -16,27 +16,38 @@ import java.util.Collection;
  */
 public class NovelWord2Vec {
     public static void main(String[] args) throws FileNotFoundException {
-        String filePath = new ClassPathResource("sanguo.txt").getFile().getAbsolutePath();
+        String filePath = new ClassPathResource("三国演义白话文.txt").getFile().getAbsolutePath();
         SentenceIterator iterator = new BasicLineIterator(filePath);
-        TokenizerFactory factory = new DefaultTokenizerFactory();
+        TokenizerFactory tokenizerFactory = new DefaultTokenizerFactory();
 
-        factory.setTokenPreProcessor(new SimpleTokenProcess());
+        tokenizerFactory.setTokenPreProcessor(new SimpleTokenProcess());
 
         Word2Vec vec = new Word2Vec.Builder()
-                .minWordFrequency(5)
+                //minWordFrequency是一个词在语料中必须出现的最少次数。
+                // 本例中出现不到五次的词都不予学习。词语必须在多种上下文中出现，
+                // 才能让模型学习到有用的特征。对于规模很大的语料库，理应提高出现次数的下限。
+                .minWordFrequency(8)
+                //iterations是网络在处理一批数据时允许更新系数的次数。迭代次数太少，网络可能来不及学习所有能学到的信息；迭代次数太多则会导致网络定型时间变长。
                 .iterations(3)
-                .learningRate(0.01)
-                .layerSize(250)
+                //learningRate是每一次更新系数并调整词在特征空间中的位置时的步幅。
+                //.learningRate(0.01)
+                //minLearningRate是学习速率的下限。学习速率会随着定型词数的减少而衰减。
+                // 如果学习速率下降过多，网络学习将会缺乏效率。这会使系数不断变化。
+                .minLearningRate(0.01)
+                //layerSize指定词向量中的特征数量，与特征空间的维度数量相等。以500个特征值表示的词会成为一个500维空间中的点。
+                .layerSize(100)
                 .seed(42)
                 .windowSize(5)
+                //iterate告知网络当前定型的是哪一批数据集。
                 .iterate(iterator)
-                .tokenizerFactory(factory)
+                //tokenizer将当前一批的词输入网络。
+                .tokenizerFactory(tokenizerFactory)
                 .build();
-
+        //vec.fit()让已配置好的网络开始定型。
         vec.fit();
 
         System.out.println(vec.similarity("诸葛亮", "孔明"));
-        System.out.println(vec.similarity("赵云", "子龙"));
+        System.out.println(vec.similarity("赵名云", "子龙"));
         Collection<String> res = vec.wordsNearest("刘备", 3);
         System.out.println(res);
     }
