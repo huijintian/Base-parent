0148e8bf89538a1aa1df3962def2a314ec1fd732
huijintian <wu750316@163.com>
2017-11-27 01:23:28 +0800

    modify

--- Algorithm/src/main/java/com/algorithm/datamining/ID3/DataNode.java
+++ Algorithm/src/main/java/com/algorithm/datamining/ID3/DataNode.java
@@ -2,6 +2,13 @@ package com.algorithm.datamining.ID3;
 
 /**
  * Created by mengtian on 2017/11/26
+ * <p>
+ * 叶子节点
  */
 public class DataNode {
+    private int dataIndex;
+
+    public DataNode(int dataIndex) {
+        this.dataIndex = dataIndex;
+    }
 }
