6cc40e4e4c0dbd4dda80c57c08d1c961750f2502
mengtian <wu750316@163.com>
2017-11-28 14:33:20 +0800

    add classifiers

---
+++ Algorithm/src/main/java/com/algorithm/classifiers/Classifier.java
@@ -0,0 +1,7 @@
+package com.algorithm.classifiers;
+
+/**
+ * Created by mengtian on 2017/11/28
+ */
+public class Classifier {
+}
---
+++ Algorithm/src/main/java/com/algorithm/classifiers/tree/TreePruner.java
@@ -0,0 +1,7 @@
+package com.algorithm.tree;
+
+/**
+ * Created by mengtian on 2017/11/27
+ */
+public class TreePruner {
+}
